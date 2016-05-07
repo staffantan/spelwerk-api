70f1c84ca3f9aac4689b7cf96a68d691364dfe5f
princejonn <jonn@jonn.nu>
2016-05-07 22:17:15 +0200

    Bugfixing Attribute

--- model/attribute.js
+++ model/attribute.js
@@ -7,9 +7,12 @@ function Attribute(router, connection) {
 
 Attribute.prototype.routes = function(router, connection) {
     router.get('/attribute', function(request, response) {
-        var query = 'SELECT * FROM attribute';
+        var query = 'SELECT attribute.*, attribute_type.name AS type, attribute_type.maximum FROM attribute ' +
+            'LEFT JOIN attribute_type ON attribute.is_attribute_type=attribute_type.id';
+
         connection.query(query, function(error, rows) {
             if(error) {
+                console.log(error);
                 response.status(400).send({error: true, message: 'error executing mysql query.', details: error});
             } else {
                 response.status(200).send({error: false, message: 'success.', result: rows})
@@ -18,25 +21,31 @@ Attribute.prototype.routes = function(router, connection) {
     });
 
     router.get('/attribute/:id', function(request, response) {
-        var query = 'SELECT * FROM attribute WHERE id = ?';
+        var query = 'SELECT attribute.*, attribute_type.name AS type, attribute_type.maximum FROM attribute ' +
+            'LEFT JOIN attribute_type ON attribute.is_attribute_type=attribute_type.id ' +
+            'WHERE attribute.id = ?';
+
         var table = [request.params.id];
         query = mysql.format(query, table);
         connection.query(query, function(error, rows) {
             if(error) {
                 response.status(400).send({error: true, message: 'error executing mysql query.', details: error});
-            } else {
+            }
+            if(rows[0] != null) {
                 response.status(200).send({error: false, message: 'success.', result: rows})
+            } else {
+                response.status(404).send({error: true, message: 'no attribute found.', details: error});
             }
         });
     });
 
     router.post('/attribute', function(request, response) {
-        var query = 'INSERT INTO attribute(name,description,attributetype) VALUES (?,?,?)';
-        var table = [request.body.name,request.body.description,request.body.type];
+        var query = 'INSERT INTO attribute(name,description,is_attribute_type,is_manifeattributeion) VALUES (?,?,?,?)';
+        var table = [request.body.name,request.body.description,request.body.attributetype,request.body.manifeattributeion];
         query = mysql.format(query, table);
         connection.query(query, function(error) {
             if (error) {
-
+                response.status(500).send({error: true, message: 'error executing mysql query.', details: error});
             } else {
                 response.status(201).send({error: false, message: 'success.'});
             }
@@ -86,7 +95,7 @@ Attribute.prototype.routes = function(router, connection) {
     /** ATTRIBUTE TYPE **/
     /** ************** **/
 
-    router.get('/attribute/type', function(request, response) {
+    router.get('/attributetype', function(request, response) {
         var query = 'SELECT * FROM attribute_type';
         connection.query(query, function(error, rows) {
             if(error) {
@@ -97,7 +106,7 @@ Attribute.prototype.routes = function(router, connection) {
         });
     });
 
-    router.get('/attribute/type/:id', function(request, response) {
+    router.get('/attributetype/:id', function(request, response) {
         var query = 'SELECT * FROM attribute_type WHERE id = ?';
         var table = [request.params.id];
         query = mysql.format(query, table);
@@ -106,25 +115,26 @@ Attribute.prototype.routes = function(router, connection) {
                 response.status(400).send({error: true, message: 'error executing mysql query.', details: error});
             } else {
                 response.status(200).send({error: false, message: 'success.', result: rows})
-            }
+            }//TODO: add if(rows[0] != null
         });
     });
 
-    router.post('/attribute/type', function(request, response) {
-        var query = 'INSERT INTO attribute(name,maximum,is_manifestation) VALUES (?,?,?)';
-        var table = [request.body.name,request.body.maximum,request.body.manifestation];
+    router.post('/attributetype', function(request, response) {
+        var query = 'INSERT INTO attribute_type(name,maximum) VALUES (?,?)';
+        var table = [request.body.name,request.body.maximum];
         query = mysql.format(query, table);
         connection.query(query, function(error) {
             if (error) {
-
+                console.log(error)
+                response.status(500).send({error: true, message: 'error executing mysql query.', details: error});
             } else {
                 response.status(201).send({error: false, message: 'success.'});
             }
         });
     });
 
-    router.put('/attribute/type/:id/name', function(request, response) {
-        var query = 'UPDATE attribute SET name = ? WHERE id = ?';
+    router.put('/attributetype/:id/name', function(request, response) {
+        var query = 'UPDATE attribute_type SET name = ? WHERE id = ?';
         var table = [request.body.name,request.params.id];
         query = mysql.format(query, table);
         connection.query(query, function(error) {
@@ -136,8 +146,8 @@ Attribute.prototype.routes = function(router, connection) {
         });
     });
 
-    router.put('/attribute/type/:id/maximum', function(request, response) {
-        var query = 'UPDATE attribute SET maximum = ? WHERE id = ?';
+    router.put('/attributetype/:id/maximum', function(request, response) {
+        var query = 'UPDATE attribute_type SET maximum = ? WHERE id = ?';
         var table = [request.body.maximum,request.params.id];
         query = mysql.format(query, table);
         connection.query(query, function(error) {
@@ -149,8 +159,8 @@ Attribute.prototype.routes = function(router, connection) {
         });
     });
 
-    router.delete('/attribute/type/:id', function(request, response) {
-        var query = 'DELETE from attribute WHERE id = ?';
+    router.delete('/attributetype/:id', function(request, response) {
+        var query = 'DELETE from attribute_type WHERE id = ?';
         var table = [request.params.id];
         query = mysql.format(query, table);
         connection.query(query, function(error) {
--- model/index.js
+++ model/index.js
@@ -1,8 +1,8 @@
+exports.Attribute = require('./attribute');
 exports.Character = require('./character');
 exports.GameModule = require('./gamemodule');
 exports.Manifestation = require('./manifestation');
 exports.Person = require('./person');
-exports.Attribute = require('./attribute');
 exports.SocialStatus = require('./socialstatus');
 exports.Species = require('./species');
 exports.User = require('./user');
